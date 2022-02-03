7af473f0342ded43e9e98137cbcfb9cd1c72ba48
SebuBergman <basti.bergman@gmail.com>
2022-02-03 13:59:43 +0200

    ekat lisäykset

---
+++ src/main/java/hh/swd20/palvelinohjelmoinnintestiprojekti/PalvelinohjelmoinnintestiprojektiApplication.java
@@ -0,0 +1,13 @@
+package hh.swd20.palvelinohjelmoinnintestiprojekti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PalvelinohjelmoinnintestiprojektiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PalvelinohjelmoinnintestiprojektiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hh/swd20/palvelinohjelmoinnintestiprojekti/PalvelinohjelmoinnintestiprojektiApplicationTests.java
@@ -0,0 +1,13 @@
+package hh.swd20.palvelinohjelmoinnintestiprojekti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PalvelinohjelmoinnintestiprojektiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
